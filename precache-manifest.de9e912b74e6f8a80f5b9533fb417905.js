4c946b804df2adb5c3f1722fb92c2870b6290091
Sohail M. Khan <sokhan@mediamath.com>
2021-08-15 03:38:06 +0800

    Updates

---
+++ precache-manifest.de9e912b74e6f8a80f5b9533fb417905.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/arabic-dict/favicon.ico"
+  },
+  {
+    "revision": "219cff609b03b2feeeb73e5aae9c2f5f",
+    "url": "/arabic-dict/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/arabic-dict/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/arabic-dict/manifest.json"
+  },
+  {
+    "revision": "02975b99a19a91f77507",
+    "url": "/arabic-dict/static/css/main.dea26371.chunk.css"
+  },
+  {
+    "revision": "02975b99a19a91f77507",
+    "url": "/arabic-dict/static/js/main.02975b99.chunk.js"
+  },
+  {
+    "revision": "ebe5c0a7d883a4ce8919",
+    "url": "/arabic-dict/static/js/runtime~main.ebe5c0a7.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/arabic-dict/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
